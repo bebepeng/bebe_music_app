cd915ec135f9d88cd18158bb7e974e3c0d3c5016
Bebe Peng <bebepeng@gmail.com>
2014-03-26 16:59:37 -0600

    songs can be added to the song list

--- app.rb
+++ app.rb
@@ -1,11 +1,22 @@
 require 'sinatra/base'
 
+SONGDATA = []
+
 class App < Sinatra::Application
   get '/' do
     erb :welcome_page
   end
 
   get '/home' do
-    erb :home_page
+    erb :home_page, :locals => {:song_data => SONGDATA}
+  end
+
+  get '/add_song' do
+    erb :new_song_form
+  end
+
+  post '/home' do
+    SONGDATA << [params[:title], params[:artist], params[:language]]
+    redirect '/home'
   end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -12,6 +12,16 @@ feature "Bebe's Favorite Music Site" do
 
     click_on "click to enter"
     expect(page).to have_content("List of Bebe's Favorite Songs")
+
+    click_on "Add to List"
+    fill_in "title", :with => "Song Title"
+    fill_in "artist", :with => "Artist Blah"
+    fill_in "language", :with => "Blah Language"
+    click_on "Submit"
+    expect(page).to have_content("Song Title")
+    expect(page).to have_content("Artist Blah")
+    expect(page).to have_content("Blah Language")
+
     #can add to the list via form
     #can sort by sort title, or artist
     #can search for specific song/artist
