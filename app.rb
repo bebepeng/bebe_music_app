da7dffc09ef8b41b4970fbb3b2445618efcc9471
Bebe Peng <bebepeng@gmail.com>
2014-03-26 16:06:44 -0600

    implemented homepage

--- app.rb
+++ app.rb
@@ -4,4 +4,8 @@ class App < Sinatra::Application
   get '/' do
     erb :welcome_page
   end
+
+  get '/home' do
+    erb :home_page
+  end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -9,6 +9,9 @@ feature "Bebe's Favorite Music Site" do
 
     visit '/'
     expect(page).to have_content("Welcome")
+
+    click_on "click to enter"
+    expect(page).to have_content("List of Bebe's Favorite Songs")
     #can add to the list via form
     #can sort by sort title, or artist
     #can search for specific song/artist
