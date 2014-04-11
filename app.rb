1ef1315f3ff48212e531f28ce480db8533b6a3fc
Bebe Peng <bebepeng@gmail.com>
2014-04-11 15:06:13 -0600

    fixed form autofill bug

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ class App < Sinatra::Application
   end
 
   get '/add_song' do
-    erb :new_song_form
+    erb :new_song_form, :locals => {:title => nil, :artist => nil, :language => nil, :album => nil, :youtube => nil}
   end
 
   get '/home/:id' do
@@ -25,7 +25,7 @@ class App < Sinatra::Application
     erb :edit_song_form, :locals => {:id => id, :title => SONGDATA.get_title(id), :artist => SONGDATA.get_artist(id), :language => SONGDATA.get_language(id), :album => SONGDATA.get_album(id), :youtube => SONGDATA.get_youtube(id)}
   end
 
-  post '/home' do
+  post '/add_song' do
     you_link = params[:youtube]
     if YoutubeValidator.valid?(you_link) || you_link.empty?
       you_link = YoutubeValidator.make_useable(you_link)
@@ -33,12 +33,7 @@ class App < Sinatra::Application
       redirect '/home'
     else
       session[:you_error] = 'Please Enter a Valid Youtube Link'
-      session[:artist] = params[:artist]
-      session[:title] = params[:title]
-      session[:language] = params[:language]
-      session[:album] = params[:album]
-      session[:youtube] = params[:youtube]
-      redirect '/add_song'
+      erb :new_song_form, :locals => {:artist => params[:artist],:title => params[:title],:language => params[:language], :album => params[:album],:youtube => you_link}
     end
   end
 
@@ -50,12 +45,7 @@ class App < Sinatra::Application
       redirect '/home'
     else
       session[:you_error] = 'Please Enter a Valid Youtube Link'
-      session[:artist] = params[:artist]
-      session[:title] = params[:title]
-      session[:language] = params[:language]
-      session[:album] = params[:album]
-      session[:youtube] = params[:youtube]
-      redirect "/home/#{params[:id]}"
+      erb :edit_song_form, :locals => {:id => params[:id], :title => params[:title], :artist => params[:artist], :language => params[:language], :album => params[:album], :youtube => you_link}
     end
   end
 end
